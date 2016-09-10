f9bc60b4572ce58f8c21cfdcbf12165ef0b77af7
Lucy Follansbee <lfollansbee@gmail.com>
2016-09-10 12:57:00 -0600

    advanced search options stored in js rather than html and ng-repeat

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,25 +4,76 @@
   MainController.$inject = ["$scope", "$http", "SearchService"];
 
   function MainController($scope, $http, SearchService){
-    // $scope.simpleSearch = true;
+
+    $scope.fieldsets={
+      diets: {
+        title: "Diet",
+        options: [
+          'Pescetarian',
+          'Vegan',
+          'Vegetarian'
+        ]
+      },
+      cuisines: {
+        title: "Cuisines",
+        options: [
+          'Chinese',
+          'Japanese',
+          'Korean',
+          'Vietnamese',
+          'Thai',
+          'Indian',
+          'British',
+          'French',
+          'Spanish',
+          'Greek',
+          'German',
+          'Italian',
+          'Mexican',
+          'Middle Eastern',
+          'American',
+          'Cajun',
+          'Southern',
+          'Caribbean',
+          'Latin American'
+        ]
+      },
+      intolerances: {
+        title: "Intolerances",
+        options: [
+          'Dairy',
+          'Egg',
+          'Gluten',
+          'Peanut',
+          'Sesame',
+          'Seafood',
+          'Shellfish',
+          'Soy',
+          'Sulfite',
+          'Tree Nut',
+          'Wheat'
+        ]
+      },
+      types: {
+        title: "Type",
+        options:[
+          'Main Course',
+          'Side Dish',
+          'Dessert',
+          'Appetizer',
+          'Salad',
+          'Bread',
+          'Breakfast',
+          'Soup',
+          'Beverage',
+          'Sauce'
+        ]
+      }
+    };
 
     $scope.search = function(query){
       SearchService.simpleSearch(query)
     }
 
-    // $http({
-    //   method:'GET',
-    //   params:{
-    //     query: 'cinnamon rolls',
-    //     type: 'breakfast',
-    //     // cuisine: 'mexican',
-    //     number: '15'
-    //   },
-    //   url: "http://localhost:3000/search"
-    // })
-    // .then(function(data){
-    //   $scope.recipes = data.data.results;
-    //   console.log($scope.recipes);
-    // })
   }
 })();
