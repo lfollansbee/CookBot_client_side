717bf24cc66086243ba5ca74481e4f952289a193
Lucy Follansbee <lfollansbee@gmail.com>
2016-09-14 11:17:42 -0600

    fetching recipe instructions with recipe id after fetching recipe details

--- components/recipe/RecipeController.js
+++ components/recipe/RecipeController.js
@@ -2,29 +2,33 @@
   angular.module("cookbookApp")
   .controller("RecipeController", RecipeController)
 
-  RecipeController.$inject = ["$scope", "$http", "$stateParams", "SpeechService"];
+  RecipeController.$inject = ["$scope", "$http", "$stateParams", "SpeechService", "$q"];
 
-  function RecipeController($scope, $http, $stateParams, SpeechService){
+  function RecipeController($scope, $http, $stateParams, SpeechService, $q){
     $scope.recipe = {}
+    $scope.instructions = [];
     $scope.recipeId = $stateParams.id;
 
-    $http({
-      method: 'GET',
-      params:{
-        id: $scope.recipeId
-      },
-      url: 'http://localhost:3000/recipeId/'
-    })
-    .then(function(response){
-      $scope.recipe = (response.data)
-      console.log($scope.recipe);
-    }, function(err){
-      return err;
+    $q.all([
+      $http({
+        method: 'GET',
+        params:{
+          id: $scope.recipeId
+        },
+        url: 'http://localhost:3000/recipeId/'
+      }),
+      $http({
+        method: 'GET',
+        params:{
+          id: $scope.recipeId
+        },
+        url: 'http://localhost:3000/recipeInstructions/'
+      })
+    ]).then(function(response) {
+      $scope.recipe = (response[0].data)
+      $scope.instructions = (response[1].data[0].steps)
+      console.log($scope.recipe, $scope.instructions);
     });
-
-    $scope.speech = function(ingredient){
-      SpeechService.textToSpeech(ingredient)
-    }
 
   }
 })();
