4339212a2efcf581a08aee41039ba44a17baa039
Lucy Follansbee <lfollansbee@gmail.com>
2016-09-17 16:33:14 -0600

    fixing edge cases in recipe views for instructions and sources

--- components/recipe/RecipeController.js
+++ components/recipe/RecipeController.js
@@ -2,9 +2,6 @@
   angular
     .module("cookbookApp")
     .controller("RecipeController", RecipeController)
-    .filter('encodeURIComponent', function() {
-    return window.encodeURIComponent;
-  });
 
   RecipeController.$inject = ["$scope", "$http", "$stateParams", "$q"];
 
@@ -13,10 +10,17 @@
     $scope.instructions = [];
     $scope.recipeId = $stateParams.id;
     $scope.areInstructions = true;
+    $scope.sourceExists = false;
 
     $scope.checkInstructions = function(array){
       if (array.length === 0){
-        $scope.areInstructions = false;
+        $scope.areInstructions === false;
+      }
+    }
+
+    $scope.checkSource = function(array){
+      if($scope.recipe.hasOwnProperty('sourceName')){
+        $scope.sourceExists = true;
       }
     }
 
@@ -38,10 +42,16 @@
         // url: 'https://cookbook-app.herokuapp.com/recipeInstructions/'
       })
     ]).then(function(response) {
+      // console.log(response);
       $scope.recipe = (response[0].data)
-      $scope.instructions = (response[1].data[0].steps)
-      $scope.checkInstructions($scope.instructions)
-      console.log($scope.recipe, $scope.instructions);
+      $scope.checkSource($scope.recipe)
+      if (response[1].data.length >= 1) {
+        $scope.instructions = (response[1].data[0].steps)
+        $scope.checkInstructions($scope.instructions)
+        console.log($scope.recipe, $scope.instructions)
+      }else{
+        $scope.areInstructions = false;
+      }
     });
 
   }
